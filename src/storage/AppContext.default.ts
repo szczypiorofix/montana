4de19adaf90e99f6afe9fef76fe9ca15d9839ba4
szczypiorofix <szczypiorofix@o2.pl>
2025-01-31 09:46:09 +0100

    Pre commit hooks test 06. Version bump

--- src/storage/AppContext.default.ts
+++ src/storage/AppContext.default.ts
@@ -2,7 +2,7 @@ import { APP_VIEW } from '../shared/enums';
 import { AppContextState } from '../shared/models';
 
 export const appContextDefault: AppContextState = {
-    version: '0.0.1',
+    version: '0.0.2',
     reload: false,
     view: APP_VIEW.HOME,
 };
